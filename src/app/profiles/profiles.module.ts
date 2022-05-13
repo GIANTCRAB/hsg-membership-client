7c6873af961010f80abe6a3c49f748b6f6d62754
Huiren Woo <giantcrabby@gmail.com>
2022-05-14 03:14:01 +0800

    Add draft for view profile

--- src/app/profiles/profiles.module.ts
+++ src/app/profiles/profiles.module.ts
@@ -2,11 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProfilesRoutingModule } from './profiles-routing.module';
 import { ViewSelfProfileComponent } from './view-self-profile/view-self-profile.component';
+import { NbCardModule, NbUserModule } from '@nebular/theme';
 
 @NgModule({
-  declarations: [
-    ViewSelfProfileComponent
-  ],
-  imports: [CommonModule, ProfilesRoutingModule],
+  declarations: [ViewSelfProfileComponent],
+  imports: [CommonModule, ProfilesRoutingModule, NbCardModule, NbUserModule],
 })
 export class ProfilesModule {}
