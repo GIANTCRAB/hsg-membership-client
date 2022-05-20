a496aa6a4b6971972c6d56a17a40232a4f147951
Huiren Woo <giantcrabby@gmail.com>
2022-05-20 18:56:58 +0800

    Add auto card flip for registration confirmation

--- src/app/registration/registration-page/registration-page.component.ts
+++ src/app/registration/registration-page/registration-page.component.ts
@@ -1,6 +1,12 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
-import { BehaviorSubject, first } from 'rxjs';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  OnDestroy,
+  OnInit,
+} from '@angular/core';
+import { BehaviorSubject, first, Subscription } from 'rxjs';
 import { ApiService } from '../../services/api.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-registration-page',
@@ -8,16 +14,32 @@ import { ApiService } from '../../services/api.service';
   styleUrls: ['./registration-page.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class RegistrationPageComponent implements OnInit {
+export class RegistrationPageComponent implements OnInit, OnDestroy {
   flipped$: BehaviorSubject<Boolean> = new BehaviorSubject<Boolean>(false);
+  routeSubscription?: Subscription;
 
-  constructor(private readonly apiService: ApiService) {}
+  constructor(
+    private readonly apiService: ApiService,
+    private readonly route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
     this.apiService
       .get('/user-auth/csrf-token')
       .pipe(first())
       .subscribe(() => {});
+
+    this.routeSubscription = this.route.params.subscribe((params) => {
+      if (params['id'] && params['id'] !== '') {
+        this.toggleFlipCard();
+      }
+    });
+  }
+
+  ngOnDestroy(): void {
+    if (this.routeSubscription) {
+      this.routeSubscription.unsubscribe();
+    }
   }
 
   toggleFlipCard() {
